98c6fc1f40f2f26104c2a517e7622c742f9766e0
Se-Chien Hsu <shsu@econsys.com>
2021-04-14 11:02:37 -0500

    add gitignore

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # econsys-pocs
+
+Front End:
+Run yarn install on top level directory
+Start front-end with "yarn run start"
+Must have parcel installed globally
+
+Start back-end with gunicorn --reload dataApi:api
+Need to install the following dependencies with pip (make sure it's python3)
+falcon, pandas, numpy, falcon_cors
--- src/components/PositionGrid.jsx
+++ src/components/PositionGrid.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import GridLayout, { WidthProvider } from 'react-grid-layout';
+import React, { useState, useEffect } from "react";
+import GridLayout, { WidthProvider } from "react-grid-layout";
 import {
   Container,
   Row,
@@ -19,76 +19,75 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
-} from 'reactstrap';
+} from "reactstrap";
 
-import './PositionGrid.css';
-import DataGrid from './DataGrid';
-
-const PositionModal = (props) => {
-  const { buttonLabel, className } = props;
-
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
-
-  return (
-    <div>
-      <Button color='danger' onClick={toggle}>
-        {buttonLabel}
-      </Button>
-      <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-          <Button color='primary' onClick={toggle}>
-            Do Something
-          </Button>{' '}
-          <Button color='secondary' onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </div>
-  );
-};
+import "./PositionGrid.css";
+import DataGrid from "./DataGrid";
 
 const DropDown = ({ dropdownOpen, toggle, sortPositions }) => (
-  <Dropdown isOpen={dropdownOpen} toggle={toggle} style={{ margin: '10px' }}>
-    <DropdownToggle color='primary' caret>
+  <Dropdown isOpen={dropdownOpen} toggle={toggle} style={{ margin: "10px" }}>
+    <DropdownToggle color="primary" caret>
       Sort By
     </DropdownToggle>
     <DropdownMenu>
       <DropdownItem
         onClick={() =>
-          sortPositions({ field: 'totalPositions', order: 'asc' })
+          sortPositions({ field: "totalPositions", order: "asc" })
         }>
         Num Position Ascending
       </DropdownItem>
       <DropdownItem
         onClick={() =>
-          sortPositions({ field: 'totalPositions', order: 'desc' })
+          sortPositions({ field: "totalPositions", order: "desc" })
         }>
         Num Positions Descending
       </DropdownItem>
       <DropdownItem
-        onClick={() => sortPositions({ field: 'vacancies', order: 'asc' })}>
+        onClick={() => sortPositions({ field: "vacancies", order: "asc" })}>
         Num Vacancies Ascending
       </DropdownItem>
       <DropdownItem
-        onClick={() => sortPositions({ field: 'vacancies', order: 'desc' })}>
+        onClick={() => sortPositions({ field: "vacancies", order: "desc" })}>
         Num Vacancies Descending
       </DropdownItem>
     </DropdownMenu>
   </Dropdown>
 );
+
+const GradeDropDown = ({ dropdownOpen, toggle, filterByGrade }) => {
+  const grades = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+  ];
+
+  return (
+    <Dropdown isOpen={dropdownOpen} toggle={toggle} style={{ margin: "10px" }}>
+      <DropdownToggle color="primary" caret>
+        Grade
+      </DropdownToggle>
+      <DropdownMenu>
+        {grades.map((grade) => (
+          <DropdownItem onClick={() => filterByGrade(grade)}>
+            {grade}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 
 const GridLayoutWithWidth = WidthProvider(GridLayout);
 
@@ -134,31 +133,31 @@ const PositionCard = ({
 
   const data = Array(totalPositions)
     .fill({
-      'First Name': 'John',
-      'Last Name': 'Doe',
-      'Position Title': positionTitle,
-      'Pay Plan': payPlan,
+      "First Name": "John",
+      "Last Name": "Doe",
+      "Position Title": positionTitle,
+      "Pay Plan": payPlan,
     })
     .map((pos, index) => ({
-      ['Status']: statusCodes[index] === 1 ? 'Active' : 'Inactive',
-      ['Budget Code']: budgetCodes[index],
+      ["Status"]: statusCodes[index] === 1 ? "Active" : "Inactive",
+      ["Budget Code"]: budgetCodes[index],
       Grade: gradeCodes[index],
       ...pos,
     }));
 
   const keys = [
-    'First Name',
-    'Last Name',
-    'Position Title',
-    'Budget Code',
-    'Pay Plan',
-    'Status',
-    'Grade',
+    "First Name",
+    "Last Name",
+    "Position Title",
+    "Budget Code",
+    "Pay Plan",
+    "Status",
+    "Grade",
   ];
 
   return (
     <React.Fragment>
-      <Modal isOpen={modal} toggle={toggle} size='lg'>
+      <Modal isOpen={modal} toggle={toggle} size="lg">
         <ModalHeader toggle={toggle}>{positionTitle}</ModalHeader>
         <ModalBody>
           <Row>
@@ -169,32 +168,32 @@ const PositionCard = ({
             heightProp={300}
             columns={keys}
             data={data}
-            rowSelection='single'
+            rowSelection="single"
             onRowDoubleClicked={(data) => console.log(data)}
             updateSelectedRows={(rows) => null}
           />
         </ModalBody>
         <ModalFooter>
-          <Button color='secondary' onClick={toggle}>
+          <Button color="secondary" onClick={toggle}>
             Close
           </Button>
         </ModalFooter>
       </Modal>
       <Card>
-        <CardHeader style={{ fontSize: '14px', fontWeight: 'bold' }}>
+        <CardHeader style={{ fontSize: "14px", fontWeight: "bold" }}>
           {positionTitle}
         </CardHeader>
-        <CardBody style={{ height: '230px' }}>
-          <CardTitle style={{ fontSize: '12px' }}>
-            <strong>Position Title Variations:</strong>{' '}
-            {positionTitleVariations.join(', ')}
+        <CardBody style={{ height: "230px" }}>
+          <CardTitle style={{ fontSize: "12px" }}>
+            <strong>Position Title Variations:</strong>{" "}
+            {positionTitleVariations.join(", ")}
           </CardTitle>
           <CardText>
             <p>
               <strong>Series:</strong> {series}
             </p>
             <p>
-              <strong>Grades:</strong> {grades.join(', ')}
+              <strong>Grades:</strong> {grades.join(", ")}
             </p>
             <p>
               <strong>Total Positions: {totalPositions}</strong>
@@ -213,7 +212,7 @@ const PositionCard = ({
             </p>
           </CardText>
         </CardBody>
-        <CardFooter style={{ textAlign: 'right' }}>
+        <CardFooter style={{ textAlign: "right" }}>
           <Button onClick={toggle}>View Positions</Button>
         </CardFooter>
       </Card>
@@ -223,6 +222,7 @@ const PositionCard = ({
 
 const PositionGrid = ({ positions }) => {
   const [dropdownOpen, setdropdownOpen] = useState(false);
+  const [gradeDropDownOpen, setGradeDropDownOpen] = useState(false);
   const [sortedPositions, setSortedPositions] = useState(positions);
 
   // x
@@ -235,7 +235,7 @@ const PositionGrid = ({ positions }) => {
 
   for (let index = 0; index < 48; index++) {
     layout.push({
-      i: 'position' + index,
+      i: "position" + index,
       x: (index % 3) * 4,
       y: Math.floor(index / 3) * 8,
       w: 4,
@@ -249,17 +249,31 @@ const PositionGrid = ({ positions }) => {
 
   const sortPositions = ({ field, order }) => {
     const sorted =
-      order === 'asc'
+      order === "asc"
         ? [...sortedPositions].sort((a, b) => a[field] - b[field])
         : [...sortedPositions].sort((a, b) => b[field] - a[field]);
 
     setSortedPositions(sorted);
   };
 
+  const filterByGrade = (grade) => {
+    const filtered = positions.filter((pos) => pos.grades.includes(grade));
+    // console.log(filtered);
+
+    setSortedPositions();
+  };
+
   return (
     <Container fluid>
       <Row>
-        <Col sm={12} style={{ textAlign: 'right' }}>
+        {/* <Col sm={4}>
+          <GradeDropDown
+            dropdownOpen={gradeDropDownOpen}
+            toggle={() => setGradeDropDownOpen(!gradeDropDownOpen)}
+            filterByGrade={(grade) => filterByGrade(grade)}
+          />
+        </Col> */}
+        <Col sm={12} style={{ textAlign: "right" }}>
           <DropDown
             dropdownOpen={dropdownOpen}
             toggle={() => setdropdownOpen(!dropdownOpen)}
@@ -270,7 +284,7 @@ const PositionGrid = ({ positions }) => {
       <Row>
         <Col>
           <GridLayoutWithWidth
-            className='layout'
+            className="layout"
             layout={layout}
             cols={12}
             rowHeight={30}
@@ -280,7 +294,7 @@ const PositionGrid = ({ positions }) => {
                 <PositionCard {...pos} />
               </div>
             ))}
-          </GridLayoutWithWidth>{' '}
+          </GridLayoutWithWidth>{" "}
         </Col>
       </Row>
     </Container>
